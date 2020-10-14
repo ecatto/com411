c0be25fcc2c35e305d7b533e02e925ca765274dd
repl.it user <replituser@example.com>
2020-10-14 16:55:48 +0000

    beep 2 again

--- main.py
+++ main.py
@@ -1,28 +1,2 @@
 print("hello world")
 
-# Ask user to enter their name
-#print("What is your name human?")
-#name = input()
-#print("It is nice to meet you human", name) 
-
-
-
-#beep  
-print("        O         ")
-print("        |         ")
-print("        |         ")
-print("    #########     ")
-print("    # 0   0 #     ")
-print("    #  [_]  #     ")
-print("    ##########    ")
-print("        ||        ")
-print("##################")
-print("# ##          ## #")
-print("# ##          ## #")
-print("##################")
-print("    ##########    ")
-print("    ##########     ")
-print("    #   ##   #     ")
-print("    #   ##   #     ")
-print("    #   ##   #     ")
-print("    ##########    ")
