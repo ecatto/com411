b97b220457bc6c6d8099a84e475aa0adbc808a22
repl.it user <replituser@example.com>
2020-10-14 16:52:54 +0000

    beep2

--- basics/output/beep.py
+++ basics/output/beep.py
@@ -1,12 +1,4 @@
-print("hello world")
-
-# Ask user to enter their name
-#print("What is your name human?")
-#name = input()
-#print("It is nice to meet you human", name) 
-
-
-
+#beep  
 #beep  
 print("        O         ")
 print("        |         ")
