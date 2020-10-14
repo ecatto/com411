1ca1cc2ea938f5e6b722120b0e0d296d213b9a2e
repl.it user <replituser@example.com>
2020-10-14 15:12:01 +0000

    escape and I am beep

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
 print("hello world")
 
-# Display escape characters
-print("\n Displays a new line")
-print("\t Displays a tab space")
-print("\\ Displays a back slash")
-print("\" Displays a double quote")
-print("\' Displays a single quote")
