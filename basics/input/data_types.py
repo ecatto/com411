ba84e5a32e47228aa954cdeee027e23d8138c6d9
repl.it user <replituser@example.com>
2020-10-15 14:02:00 +0000

    data_types

---
+++ basics/input/data_types.py
@@ -0,0 +1,15 @@
+
+#ask user name
+What is your name human?
+name =input()
+
+How old are you (in years)?
+age = input()
+
+How tall are you (in meters)?
+height = input ()
+
+How much do you weigh (in kilograms)?
+85
+
+Prins you are 36 years old and your bmi is 26.23.
