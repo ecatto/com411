784b6bc45916e4a0328dbd8a375386e2b7b37e05
repl.it user <replituser@example.com>
2020-10-16 16:53:25 +0000

    data_types2

---
+++ basics/input/data_types2.py
@@ -0,0 +1,21 @@
+#ask user name
+print("What is your name human?")
+name =input()
+
+#ask user age
+print("How old are you (in years)?")
+age = input()
+
+#ask user height
+print("How tall are you (in meters)?")
+height = float(input())
+
+#ask user weight
+print("How much do you weigh (in kilograms)?")
+weight =float(input())
+
+#calculate bmi
+bmi=weight/(height*height)
+
+#display user info
+print(str(name),"you are",int(age),"years old and your bmi is",round(bmi,2))
