e6ce83b09756b1ee87a507dec20ed30e05e0e4e3
repl.it user <replituser@example.com>
2020-10-22 13:40:35 +0000

    comparison

---
+++ basics/decisions/comparison_operator.py
@@ -0,0 +1,12 @@
+print("Please enter the first number")
+num_one = int(input())
+
+print("Please enter the second number")
+num_two = int(input())
+
+if (num_one < num_two) :
+  print("The first number is the smallest")
+elif (num_one > num_two) :
+  print("The second number is the smallest")
+else :
+  print("Both numbers are equal")
