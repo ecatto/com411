0995b959fe7c64b48045c2b098f2cadf43913e6b
repl.it user <replituser@example.com>
2020-10-23 22:26:37 +0000

    and_operator

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,12 @@
+
+print("What did I hear?")
+hear = input()
+
+print("What did I see?")
+response = input()
+
+if (response.lower() == "two red eyes") and (hear.lower() =="grrr"):
+  print("There is a scary creature. I should get out of here!")
+
+else :
+  print("I am a little scared but I will continue")
