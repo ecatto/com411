9f17f55e71b052baab5873559faa291aed0e54fb
repl.it user <replituser@example.com>
2020-10-25 16:01:46 +0000

    count_loop

---
+++ basics/repetitions/while_loop/count.py
@@ -0,0 +1,17 @@
+#The program should start by displaying the message "How many live cables must I avoid?".
+#The program should then read in the user's response.
+
+print("How many live cables must I avoid?")
+live = int(input())
+
+#The program should then create a variable to track the number of live cables and set this to 0.
+count= int(0)
+
+#The program should then use a while loop 
+while (count < live) :
+  count = count +1
+  print("Avoiding...Done! {} live cables avoided".format(count))
+
+#Finally, the program should display the message "All live cables have been avoided."
+
+print("\nAll live cables have been avoided")
