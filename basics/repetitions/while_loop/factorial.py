6c80e2fbcba330c4b4ae808cf85e37997273ec1f
repl.it user <replituser@example.com>
2020-10-25 19:52:35 +0000

    factorial

---
+++ basics/repetitions/while_loop/factorial.py
@@ -0,0 +1,11 @@
+print("Please enter a number:")
+answer = int(input())
+
+count =0
+total =1
+
+while (count <answer):
+  count =count +1
+  total = total *count
+
+print(total)
---
+++ basics/repetitions/while_loop/sum_user_numbers.py
@@ -0,0 +1,13 @@
+print("How many numbers should I sum up?")
+answer = int(input())
+
+count = 1
+total = 0
+
+while (count <= answer):
+  print("please enter number {} of {}".format(count, answer))
+  reply =int(input())
+  total = total + reply
+  count = count +1
+
+print("The total is", total)
