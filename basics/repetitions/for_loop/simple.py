37faae23c1d9b622335a28daa6309be23f763573
repl.it user <replituser@example.com>
2020-10-25 23:17:59 +0000

    mountains

---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,12 @@
+
+print("How many mountains should I display?")
+reply = int(input())
+
+for count in range(0,2,1) :
+  print("""             __
+  \n            /  \_  
+  \n           /^    \\
+  \n          /  ^    \\_
+  \n        _/ ^ ^     ^\\
+  \n        /  ^     ^    \\
+  """)
