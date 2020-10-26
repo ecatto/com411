55fddbd10b8b83ffe4612f7da1ae8d4773cb803f
repl.it user <replituser@example.com>
2020-10-26 13:49:30 +0000

    membership

---
+++ basics/repetitions/for_loop/membership_operators.py
@@ -0,0 +1,12 @@
+The program should begin by displaying the message "What phrase do you see?".
+The program should then read in the user's response
+Finally, the program should use a for loop with an appropriate membership operator to display the phrase in reverse.
+
+An example run of such a program is shown below:
+
+What phrase do you see?
+sucoP sucoH
+ 
+Reversing...
+
+The phrase is: Hocus Pocus
