08fc09874e3e68419fe6b71e769fe0befd369fd2
repl.it user <replituser@example.com>
2020-11-08 18:17:16 +0000

    nesting

---
+++ basics/repetitions/for_loop/reverse.py
@@ -0,0 +1,8 @@
+#The program should begin by displaying the message
+print("What phrase do you see?")
+phrase = input()
+
+print("reversing...")
+
+for count in range (len(phrase)-1,-1,-1):
+  print (phrase[count], end="")
