d7c83646c2e2878b8de883d65c886384ba1e1110
repl.it user <replituser@example.com>
2020-11-08 22:09:23 +0000

    nesting2

---
+++ basics/repetitions/nested_loop/nesting2.py
@@ -0,0 +1,25 @@
+print("Please enter a sequence of characters consisting of dashes and two markers")
+seq = input()
+
+print("Please enter the character for the marker")
+marker = input()
+print("")
+
+isFirst = True
+marker1 = 0
+marker2 = 0
+for count in range (0,len(seq),1):
+  if seq[count] == marker :
+    if isFirst == True :
+      isFirst= False
+      marker1 = count
+
+    else:
+      marker2 = count
+      
+print("marker1 is", marker1)
+print("marker2 is", marker2)
+distance=0
+distance = (marker2-marker1)-1
+
+print("The distance between the markers is ", distance)
