73a5b84488e786747d6648adf13962a0188effc0
repl.it user <replituser@example.com>
2020-11-08 22:38:15 +0000

    funct_asc

---
+++ basics/functions/ascii.py
@@ -0,0 +1,11 @@
+print("Program Started!")
+print("Please enter a standard character:")
+character = input()
+
+if len(character) == 1 :
+  print("The ASCII code for {} is {}".format(character,ord(character)))
+
+else :
+  print("Please enter a single character")
+
+print("Program Ended!")
