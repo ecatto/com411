e3d5cc5c3e495680f374d04f75d6a41f43f6c10e
repl.it user <replituser@example.com>
2020-11-12 21:20:38 +0000

    week3

--- basics/functions/function_with_loop.py
+++ basics/functions/function_with_loop.py
@@ -1,17 +1,11 @@
 def cross_bridge(steps):
-  for count in range (0,steps,1) :
+  for count in range (steps) :
     print("Crossed step")
   
-  if steps>4 :
+  if steps>5 :
     print("The bridge is collapsing!")
   else:
     print("we must keep going!")
 
 cross_bridge(3)
 cross_bridge(6)
-
-#The function should do the following
- #   Display the message "Crossed step" for each step crossed
-  #  If the distance is more than 5 then the message "The bridge is collapsing!" should be displayed.
-   # Otherwise the message "we must keep going!" should be displayed.
-
---
+++ basics/functions/multiple_functions.py
@@ -0,0 +1,15 @@
+def display_ladder(steps):
+  for count in range (steps):
+    print("*****")
+    print("*   *")
+    print("*   *")
+
+
+def create_ladder():
+  print("Please enter the number of steps")
+  num_steps = int(input())
+
+  display_ladder(num_steps)
+
+create_ladder()
+
