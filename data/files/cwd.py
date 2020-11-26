c21eae9e4538c961ade82c6cce140495d82d1128
repl.it user <replituser@example.com>
2020-11-26 22:07:56 +0000

    cwd

---
+++ data/files/cwd.py
@@ -0,0 +1,13 @@
+def cwd():
+  import os
+  path = os.getcwd()
+  print(f"Current Working Directory: {path}")
+  print("The directory contains the following:")
+  for file in os.listdir(path):
+    print(file)
+
+def run():
+  print("Processing...")
+  cwd()
+
+run()
